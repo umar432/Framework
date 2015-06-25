2928acfa82cf5001bd609def3790d6e548f2e2da
umar432 <umar432@gmail.com>
2015-06-25 17:10:39 +0200

    Update Last  25 2015 June

--- js/script.js
+++ js/script.js
@@ -4,12 +4,18 @@
 /* jQuery for page scrolling feature - requires jQuery Easing plugin */
 $(function() 
 {
-    $('a').bind('click', function(event) {
+    $('a').bind('click', function(event) 
+	{
 		
         var $anchor = $(this);
-        $('html, body').stop().animate({
+        $('html, body').stop().animate(
+		{
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+		
     });
+	
+
 });
+
